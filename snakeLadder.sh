78d3a6d34def0ec442f76fb85a211fd82dd0a3e8
suraj7852 <surajpj7852@gmail.com>
2019-11-14 17:52:45 +0530

    Added functionality for exact winning position to 100

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -4,6 +4,8 @@ echo "Welcome to Snake & Ladder Simulator"
 
 PLAYER=1;
 position=0;
+winCount=0;
+prevWinCount=0;
 
 function positionChk() {
     rolls=$(( RANDOM%6 +1 ))
@@ -22,13 +24,17 @@ function positionChk() {
 
 while [ true ]
 do
+    #prevWinCount=$winCount
     positionRecieved=$( positionChk )
     winCount=$(( $winCount+$positionRecieved ))
     if [ $winCount -gt 100 ]
-	then
-		break;
+    then
+        winCount=$(( $wincount-$positionRecieved ))
     elif [ $winCount -lt 0 ]
     then
         winCount=0;
-	fi
+    elif [ $winCount -eq 100 ]
+    then
+        break;
+    fi
 done
