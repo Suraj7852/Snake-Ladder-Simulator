284d0bf80c34008786b6a6b665f83933c20949b1
suraj7852 <surajpj7852@gmail.com>
2019-11-14 16:19:07 +0530

    Added Check option for No play, Snake, Ladder

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -3,6 +3,19 @@
 echo "Welcome to Snake & Ladder Simulator"
 
 PLAYER=1;
-POSITION=0;
+position=0;
 
 rolls=$(( RANDOM%6 +1 ))
+option=$(( RANDOM%3 ))
+if [ $option -eq 0 ]
+then
+    position=$(( $position ))
+    echo "No play"
+elif [ $option -eq 1 ]
+then
+    position=$(( $position + $rolls ))
+    echo "Snake"
+else
+    position=$(( $position - $rolls ))
+    echo "Ladder"
+fi
