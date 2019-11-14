45622c1ae2610cb3b46aabda69707f3da545eb74
suraj7852 <surajpj7852@gmail.com>
2019-11-14 16:44:45 +0530

    Added player winning position

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -5,17 +5,28 @@ echo "Welcome to Snake & Ladder Simulator"
 PLAYER=1;
 position=0;
 
-rolls=$(( RANDOM%6 +1 ))
-option=$(( RANDOM%3 ))
-if [ $option -eq 0 ]
-then
-    position=$(( $position ))
-    echo "No play"
-elif [ $option -eq 1 ]
-then
-    position=$(( $position + $rolls ))
-    echo "Snake"
-else
-    position=$(( $position - $rolls ))
-    echo "Ladder"
-fi
+while [ true ]
+do
+    rolls=$(( RANDOM%6 +1 ))
+    option=$(( RANDOM%3 ))
+    if [ $option -eq 0 ]
+    then
+        position=$(( $position ))
+        echo "No play"
+    elif [ $option -eq 1 ]
+    then
+        position=$(( $position + $rolls ))
+        echo "Snake"
+    else
+        position=$(( $position - $rolls ))
+        echo "Ladder"
+    fi
+    
+    if [ $position -gt 100 ]
+	then
+		break;
+    elif [ $position -lt 0 ]
+    then
+        position=0;
+	fi
+done
